7c4e86bbdf1f5ed07abd46dca19886431b781811
CarterBalow <caba8627@colorado.edu>
2026-08-31 17:32:11 -0600

    commenting

--- APPM4560_HW1_Problem6.m
+++ APPM4560_HW1_Problem6.m
@@ -1,20 +1,28 @@
 clear; clc; close all;
 
+% derived value
 analytical_val = 23 * pi / 192;
 
+% MC set-up 
 N_vals = round(logspace(2, 7, 100));
 mc_estimates = zeros(size(N_vals));
 
+% MC sim
 for i = 1 : length(N_vals)
     N = N_vals(i);
+
+    % make N rand vals for each X/Y/Z
     X = rand(N, 1);
     Y = rand(N, 1);
     Z = rand(N, 1);
 
+    % bool calculation
     mc_bool = (X.^2 + Y.^2 < Z) & (Z.^2 > X .* Y);
+    % average results
     mc_estimates(i) = sum(mc_bool) / N;
 end
 
+% plotting
 fig = figure();
 fig.Theme = 'light';
 hold on;
